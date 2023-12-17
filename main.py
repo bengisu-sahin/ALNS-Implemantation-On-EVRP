931fbc5acebfa43088a621886e16958dc92e6829
yagizharman <yagizharman4biz@gmail.com>
2023-12-18 00:16:29 +0300

    hotfix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from alnsSolution import alns_iterate
 from initialsolution import initial_solution
 from readProblemInstances import readProblemInstances
 from test_funcs import test_files_in_directory
-from visualize_solution import visualizeAllRoutes, visualizeRoutesSeperately
+from visualize_solution import visualizeAllRoutes, visualizeRoutesSeperately, writeSolution
 from AlnsOperators.CustomerOperators import Regret_K_Insertion, removeRandomCustomerOperator, leastTimeWindowCustomerRemovalOperator, relatedCustomerRemovalOperator,greedyCustomerInsertionOperator, worstDistanceCustomerRemovalOperator
 from AlnsOperators.StationOperators import Compare_K_Insertion, bestStationInsertionOperator, randomStationRemovalOperator, worstChargeUsageStationRemovalOperator, worstStationRemovalOperator
 
@@ -21,6 +21,7 @@ def main():
     alns_solution=alns_iterate(solution,j,maxIterations,N,K,Z)
     visualizeAllRoutes(alns_solution,problemFile)
     visualizeRoutesSeperately(alns_solution,problemFile)
+    writeSolution(alns_solution,problemFile)
 
 
     
