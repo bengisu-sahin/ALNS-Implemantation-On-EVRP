67bee2205b0011e853830085d1a8fc30b59cc767
yagizharman <yagizharman4biz@gmail.com>
2023-12-24 17:42:37 +0300

    small fixes

--- main.py
+++ main.py
@@ -7,22 +7,20 @@ from pandas import DataFrame
 
 def main():
     #GLOBAL VARIABLES FOR ALNS
-    j=5 #- Number of iterations allowed without improvement
-    maxIterations=50 # - Maximum number of iterations
-    N = maxIterations*0.1 # - Maximum iterations allowed without improvement
-    K= 4# Predefined iteration interval
-    Z=2 #Weights update interval
+    j=0 #- Number of iterations allowed without improvement
+    maxIterations=5000 # - Maximum number of iterations
+    N =100  # - Maximum iterations allowed without improvement
+    K= 50# Predefined iteration interval
+    Z=50
     folder_path = 'SchneiderData/'
     results = []
-    i=0
+   
     for filename in os.listdir(folder_path):
         if filename.endswith('.txt'):
             file_path = os.path.join(folder_path, filename)
             result,evrtpw_verifier_result = process_test_file(file_path,j,maxIterations,N,K,Z)
             results.append({'File': filename, 'Objective Function Value': result.get_Total_Objective_Function_Value(),"Total Distance":result.getTotalDistance(),"Evrtpw Verifier Result":evrtpw_verifier_result,'j':j,'maxIterations':maxIterations,'N':N,'K':K,'Z':Z})
-        i+=1
-        if i==2:
-            break
+
 
     results_df = pd.DataFrame(results)
     output_excel_path = 'SolutionFiles/results.xlsx'
